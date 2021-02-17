2a478e1ffb580c862d3b641143ff9e6b11561c97
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-17 16:44:00 +0000

    Updated libft

---
+++ libft/inc/ft_stdio.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stdio.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 20:15:07 by mgueifao          #+#    #+#             */
+/*   Updated: 2021/02/13 20:21:50 by mgueifao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STDIO_H
+# define FT_STDIO_H
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
